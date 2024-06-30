094f834bf52341aff026d48d88e1f4f35608e9a8
Kevin Lin <04kevinlin@gmail.com>
2024-06-30 21:28:00 +1000

    added covariance function

--- teamName.py
+++ teamName.py
@@ -9,6 +9,23 @@ import numpy as np
 nInst = 50
 currentPos = np.zeros(nInst)
 
+# Function that finds a pair with the greatest correlation(covariance closest to 1) 
+def getPair(prchistory):
+
+    price_cov = np.cov(prchistory, rowvar=False)
+
+    stock_a = 0
+    stock_b = 0
+    curr = abs(price_cov[0][0] - 1)
+
+    for i in range(0,50):
+        a = price_cov[i, : ]
+        i2 = np.argmin(a)
+        if (abs(price_cov[i][i2] < curr)):
+            stock_a = i
+            stock_b = i2
+
+    return (stock_a, stock_b)
 
 def getMyPosition(prcSoFar):
     global currentPos
@@ -21,3 +38,4 @@ def getMyPosition(prcSoFar):
     rpos = np.array([int(x) for x in 5000 * lastRet / prcSoFar[:, -1]])
     currentPos = np.array([int(x) for x in currentPos+rpos])
     return currentPos
+
